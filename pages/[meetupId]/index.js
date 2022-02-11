cf5411fc05683b4715f2c242ca8cdf89211c24d9
alisdemir33 <alisdemir33@gmail.com>
2022-02-11 15:36:56 +0300

    dd

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -4,7 +4,7 @@ import MeetupDetail from "../../components/meetups/MeetupDetail"
 const MeetupDetailPage = () =>{
 
     return <MeetupDetail 
-    title ='A First Meetup'
+    title ='A First Meetup '
     image = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'
     address ='Some address 5, 12345 Some City'
     description ='This is a first meetup!'    
